09a1f96fb264feaf107e69534cb1de602ea11910
TallFly <tallfly@vk.com>
2023-01-08 13:33:31 +0600

    changed positions. APP WORKS YAY

--- EasyRPC(NoGUI).py
+++ EasyRPC(NoGUI).py
@@ -1,17 +1,19 @@
 from pypresence import Presence
 import time
+import os
 
 clientID = input('Введите ваш Client ID: ')
 RPC = Presence(clientID)
 
-state = input('Введите название Rich Presence: ')
-details = input('Введите описание Rich Presence: ')
+pid = os.getpid()
+details = input('Введите название Rich Presence: ')
+state = input('Введите описание Rich Presence: ')
 labelofbutton = input('Введите название кнопки Rich Presence: ')
 urlofbutton = input('Куда ведет ваша кнопка: ')
 
 RPC.connect()
 
-RPC.update(state, details, buttons=[{"label": labelofbutton, "url": urlofbutton}])
+RPC.update(pid, state, details, buttons=[{"label": labelofbutton, "url": urlofbutton}])
 
 while True:
     time.sleep(15)
